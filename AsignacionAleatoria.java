5b1ab15c16d88f2964b4add0424fdd8a1199b5d1
icufaa <129627437+icufaa@users.noreply.github.com>
2024-10-13 13:03:02 -0600

    Comentarios

--- AsignacionAleatoria.java
+++ AsignacionAleatoria.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class AsignacionAleatoria implements EstrategiaAsignacionTareas {
     @Override
     public void asignarTarea(Tarea tarea, List<Empleado> empleados) {
-        // Lógica para asignar la tarea de forma aleatoria
+        //asignar la tarea de forma aleatoria
         if (!empleados.isEmpty()) {
             int index = (int) (Math.random() * empleados.size());
             tarea.asignarEmpleado(empleados.get(index));
